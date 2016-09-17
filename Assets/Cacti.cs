b33b6de3a35240404ff86a08692ad0f7cfaec68b
Leoariel <leoroberts141@gmail.com>
2016-09-17 18:46:09 -0400

    Finished cacti animations

---
+++ Assets/Cacti.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cacti : MonoBehaviour
+{
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
