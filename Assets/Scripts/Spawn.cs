2849f6a6860fa41111b6fcdee8a83c5b75ffbf70
Leoariel <leoroberts141@gmail.com>
2016-09-17 16:08:32 -0400

    Second spawn attempt

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Spawn : NetworkBehaviour
+{
+	//Camera camera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {	
+			transform.position = new Vector3 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y, 0);
+			print (Camera.main.ScreenToWorldPoint (Input.mousePosition));
+		}
+	}
+
+	public override void OnStartLocalPlayer ()
+	{
+		GetComponent<SpriteRenderer> ().material.color = Color.blue;
+	}
+}
