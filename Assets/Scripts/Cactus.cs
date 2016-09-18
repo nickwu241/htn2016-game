3e2778e0f160f12df354bed8d8be832423955005
Leoariel <leoroberts141@gmail.com>
2016-09-18 01:40:09 -0400

    Attempted cactus and title screen stuff

---
+++ Assets/Scripts/Cactus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cactus : MonoBehaviour
+{
+	//public Sprite alive;
+	public Sprite burnt;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//GetComponent<SpriteRenderer> ().sprite = alive;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//print (col.gameObject.name + "you bad");
+		if (other.gameObject.tag.Equals ("Flame")) {
+			GetComponent<SpriteRenderer> ().sprite = burnt;
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,61 +7,61 @@ public class Player : MonoBehaviour
 	public int FLAME_DURATION;
 	public int SPEED_MULTIPLIER;
 	private static Vector2 KB_VECT = Vector2.left;
+
 	public int health { get; set; }
+
 	private int flameTimer;
 	private float startTime;
+
 	public bool dead { get; set; }
+
 	public bool onFire { get; set; }
+
 	private Vector2 velocity;
 	private Rigidbody2D rb2D;
 
-	void Start()
+	void Start ()
 	{
 		health = START_HEALTH;
 		flameTimer = 0;
-		rb2D = GetComponent<Rigidbody2D>();
-		velocity = new Vector2(1, -rb2D.gravityScale * rb2D.mass / 9.8f);
+		rb2D = GetComponent<Rigidbody2D> ();
+		velocity = new Vector2 (1, -rb2D.gravityScale * rb2D.mass / 9.8f);
 		dead = false;
 		onFire = false;
 	}
 
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
-		if (health <= 0) dead = true;
-		if (onFire && (Time.time - startTime) > 1)
-		{
+		if (health <= 0)
+			dead = true;
+		if (onFire && (Time.time - startTime) > 1) {
 			health--;
 			startTime++;
 			flameTimer++;
-			if (flameTimer >= FLAME_DURATION)
-			{
+			if (flameTimer >= FLAME_DURATION) {
 				onFire = false;
 			}
 		}
 
-		if (!dead)
-		{
-			rb2D.MovePosition(rb2D.position + velocity * SPEED_MULTIPLIER * Time.fixedDeltaTime);
+		if (!dead) {
+			rb2D.MovePosition (rb2D.position + velocity * SPEED_MULTIPLIER * Time.fixedDeltaTime);
 		}
 	}
-	void OnCollisionEnter2D(Collision2D col)
+
+	void OnCollisionEnter2D (Collision2D col)
 	{
 		string colTag = col.gameObject.tag;
-		if (colTag.Equals("Enemy"))
-		{
+		if (colTag.Equals ("Enemy")) {
 			health--;
-			rb2D.MovePosition(rb2D.position + KB_VECT * SPEED_MULTIPLIER * Time.fixedDeltaTime);
-		}
-		else if (colTag.Equals("Rain"))
-		{
+			rb2D.MovePosition (rb2D.position + KB_VECT * SPEED_MULTIPLIER * Time.fixedDeltaTime);
+		} else if (colTag.Equals ("Rain")) {
 			health++;
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
+	void OnTriggerEnter2D (Collider2D other)
 	{
-		if (other.gameObject.tag.Equals("Flame"))
-		{
+		if (other.gameObject.tag.Equals ("Flame")) {
 			health--;
 			startTime = Time.time;
 			onFire = true;
