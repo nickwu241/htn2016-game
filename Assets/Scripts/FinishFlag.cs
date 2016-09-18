003cc0b63f67d9b7eb04dbc8e2a9a5ad95d3ccf5
Nicholas Wu <nickwu241@gmail.com>
2016-09-18 05:32:44 -0700

    Submission

---
+++ Assets/Scripts/FinishFlag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishFlag : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		string colTag = col.gameObject.tag;
+		if (colTag.Equals("Player"))
+		{
+			col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+		}
+	}
+}
