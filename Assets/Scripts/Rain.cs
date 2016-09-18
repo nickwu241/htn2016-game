f6b9f017ea0d63596b95ea263ef8935fa3046e17
Leoariel <leoroberts141@gmail.com>
2016-09-17 23:20:09 -0400

    Added background and water balloon animation

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rain : MonoBehaviour {
+public class Rain : MonoBehaviour
+{
 
-	void OnCollisionEnter2D(Collision2D col)
+	public Sprite pop;
+
+	void OnCollisionEnter2D (Collision2D col)
 	{
-		if (this.gameObject.tag != col.gameObject.tag)
-		{
-			Destroy(this.gameObject);
+		if (this.gameObject.tag != col.gameObject.tag) {
+			GetComponent <SpriteRenderer> ().sprite = pop;
+			Destroy (this.gameObject, 0.5f);
 		}
 	}
 }
